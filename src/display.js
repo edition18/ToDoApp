e881e74f5a34a1afc829696134a17f2bafc13938
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-12-19 22:28:11 +0800

    first push

---
+++ src/display.js
@@ -0,0 +1,41 @@
+export const projectForm = () => {
+  clearDisplay();
+  let formGroup = formGroupCreate();
+  formGroup.appendChild(
+    formTextElementCreate("projectName", "enter your project name")
+  );
+};
+
+// title, description, dueDate, priority, notes, checklist
+
+export const toggleActive = (element) => {
+  // note the use of .classList method
+  element.classList.contains("active")
+    ? element.classList.remove("active")
+    : element.setAttribute("class", "active");
+};
+
+export const createLabel = (id, name) => {
+  let label = document.createElement("label");
+  label.setAttribute("for", `${id}`);
+  label.innerHTML = `${name}`;
+  return label;
+};
+
+export const formGroupCreate = () => {
+  let formGroup = document.createElement("div");
+  formGroup.setAttribute("class", "form-group");
+  return formGroup;
+};
+
+export const formTextElementCreate = (id, placeholder) => {
+  let textElement = document.createElement("input");
+  textElement.setAttribute("type", `text`);
+  textElement.setAttribute("id", `${id}`);
+  textElement.setAttribute("placeholder", `${placeholder}`);
+  return textElement;
+};
+
+export const clearDisplay = () => {
+  document.getElementById("display").innerHTML = ``;
+};
